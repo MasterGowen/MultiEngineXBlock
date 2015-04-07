541d0e5d54a86406190f2821d7a062ed34eed66f
MasterGowen <mastergowen@gmail.com>
2015-04-07 18:18:43 +0500

    tinymce

--- multiengine/static/js/src/multiengine_edit.js
+++ multiengine/static/js/src/multiengine_edit.js
@@ -3,7 +3,7 @@ function MultiEngineXBlockEdit(runtime, element) {
     var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
     var data = {
       display_name: $(element).find('input[name=display_name]').val(),
-      question: $(element).find('input[name=question]').val(),
+      question: $(element).find('textarea[id=question-area]').val(),
     };
             $.post(handlerUrl, JSON.stringify(data)).done(function(response) {
       window.location.reload(false);
@@ -14,14 +14,6 @@ function MultiEngineXBlockEdit(runtime, element) {
     runtime.notify('cancel', {});
   });
 
-  var area = document.getElementById('question-area');
-    if (area.addEventListener) {
-        area.addEventListener('input', function() {
-            document.getElementById('question').value = area.value;
-        }, false);
-    } else if (area.attachEvent) {
-        area.attachEvent('onpropertychange', function() {
-        document.getElementById('question').value = area.value;
-        });
+  tinyMCE.init({delector:"#question-area"});
     }
 }
