9a22622398f91cd17ecb80d263e73fb100e26210
vesloguzov <vesloguzov@gmail.com>
2015-04-07 19:21:43 +0500

    - default css&js

--- multiengine/static/js/src/multiengine.js
+++ multiengine/static/js/src/multiengine.js
@@ -1,22 +1,7 @@
 /* Javascript for MultiEngineXBlock. */
 function MultiEngineXBlock(runtime, element) {
 
-    function updateCount(result) {
-        $('.count', element).text(result.count);
-    }
-
-    var handlerUrl = runtime.handlerUrl(element, 'increment_count');
-
-    $('p', element).click(function(eventObject) {
-        $.ajax({
-            type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify({"hello": "world"}),
-            success: updateCount
-        });
-    });
-
-    $(function ($) {
+     $(function ($) {
         /* Here's where you'd do things on page load. */
     });
 }
