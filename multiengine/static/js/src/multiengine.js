ebe599db4a9f66a523e40e572f90e57f0f56e915
vesloguzov <vesloguzov@gmail.com>
2015-04-09 20:31:34 +0500

    from_student

--- multiengine/static/js/src/multiengine.js
+++ multiengine/static/js/src/multiengine.js
@@ -1,7 +1,21 @@
 /* Javascript for MultiEngineXBlock. */
 function MultiEngineXBlock(runtime, element) {
 
-     $(function ($) {
-        /* Here's where you'd do things on page load. */
-    });
+    function success_func(result) {
+    		console.log("Correct: " + result.correct)
+        //$('.count', element).text(result.count);
+    }
+var handlerUrl = runtime.handlerUrl(element, 'student_submit');
+
+  $(element).find('.save').bind('click', function() {
+    var data = {
+      answer: $(element).find('textarea[id=answer]').val(),
+    };
+            $.ajax({
+            type: "POST",
+            url: handlerUrl,
+            data: JSON.stringify(data),
+            success: success_func 
+        });
+  });
 }
