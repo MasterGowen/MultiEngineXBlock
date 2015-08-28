ad15c002339bdd60f80c1addaa5c79a992559fc8
MasterGowen <mastergowen@gmail.com>
2015-08-28 16:13:35 +0500

    add LOCALE_PATHS

--- multiengine/settings.py
+++ multiengine/settings.py
@@ -3,10 +3,7 @@ USE_I18N=True
 USE_L10N = True
 TIME_ZONE = 'Asia/Yekaterinburg'
 LANGUAGE_CODE = 'ru-RU'
-LANGUAGES = (
-	('ru', u'Русский'),
-	('en@lolcat', u'LOLCAT English'),
-    ('en@pirate', u'Pirate English'),
-    )
-LANGUAGE_DICT = dict(LANGUAGES)
+
+PROJECT_ROOT = path(__file__).abspath().dirname()
+LOCALE_PATHS = (PROJECT_ROOT + '/conf/locale',)
 GIT_REPO_URL = 'https://github.com/MasterGowen/multiengine-scenarios.git'
