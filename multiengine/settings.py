f3c1b13dcb51c49a4ffad700881b7fecc17fefe9
MasterGowen <mastergowen@gmail.com>
2015-08-28 15:58:49 +0500

    settings

--- multiengine/settings.py
+++ multiengine/settings.py
@@ -1,1 +1,12 @@
+
+USE_I18N=True
+USE_L10N = True
+TIME_ZONE = 'Asia/Yekaterinburg'
+LANGUAGE_CODE = 'ru-RU'
+LANGUAGES = (
+	('ru', u'Русский'),
+	('en@lolcat', u'LOLCAT English'),
+    ('en@pirate', u'Pirate English'),
+    )
+LANGUAGE_DICT = dict(LANGUAGES)
 GIT_REPO_URL = 'https://github.com/MasterGowen/multiengine-scenarios.git'
