867d28e684582016c5ac50151aad090bcbae477b
MasterGowen <mastergowen@gmail.com>
2015-09-03 16:06:30 +0500

    auto remove span.saved

--- multiengine/static/js/multiengine.js
+++ multiengine/static/js/multiengine.js
@@ -70,6 +70,7 @@ function MultiEngineXBlock(runtime, element) {
     function success_save(result){
 
         $(element).find('.save_button').append('<span class="saved">Сохранено</span>');
+        setTimeout($(element).find('.saved').remove(), 3000);
     }
 
     var handlerUrl = runtime.handlerUrl(element, 'student_submit');
