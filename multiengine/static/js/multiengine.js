50903bcd4ab5eed1bb0a2cbdef47d5cb97a0b7f9
MasterGowen <mastergowen@gmail.com>
2015-09-08 15:29:23 +0500

    make love not var

--- multiengine/static/js/multiengine.js
+++ multiengine/static/js/multiengine.js
@@ -166,7 +166,8 @@ function MultiEngineXBlock(runtime, element) {
     var studentState = getStudentState(getStudentStateURL),
         scenario = getScenario(scenarioURL),
         scenarioJSON = JSON.parse(scenario),
-        uniqueId = elementDOM.getAttribute('data-usage-id'),
+        uniqueId = elementDOM.getAttribute('data-usage-id');
+        
         mengine.studentStateJSON = getStudentState(getStudentStateURL);
         console.log(mengine.studentStateJSON);
 
