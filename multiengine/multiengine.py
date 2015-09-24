e5e5a3d85886239d02e9c01c5200d7e02c7a54ec
MasterGowen <mastergowen@gmail.com>
2015-09-24 17:33:52 +0500

    float points & 0_1 weight

--- multiengine/multiengine.py
+++ multiengine/multiengine.py
@@ -641,10 +641,9 @@ class MultiEngineXBlock(XBlock):
             def _result_postproduction(result):  # , settings['postproduction_rule']=None):
                 result = int(round(result * self.weight))
                 self.runtime.publish(self, 'grade', {
-                    'value': self.points,
-                    'max_value': self.weight,
+                    'value': float(self.weight/self.points),
+                    'max_value': 1,  # self.weight,
                 })
-                self.runtime.publish(self, 'progress', {})
                 return result
 
             if settings['sequence'] is True:
