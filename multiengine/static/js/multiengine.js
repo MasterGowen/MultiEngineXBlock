5ab8dd568df86d42c0beafdc82ee6e528fc12dbb
MasterGowen <mastergowen@gmail.com>
2015-09-29 17:27:38 +0500

    $('.max_points', element).text(result.weight);

--- multiengine/static/js/multiengine.js
+++ multiengine/static/js/multiengine.js
@@ -123,8 +123,9 @@ function MultiEngineXBlock(runtime, element) {
     function success_func(result) {
         //console.log("Количество баллов: " + result.correct/result.weight*100 + " ОТВЕТОВ: " + result.attempts);
         $('.attempts', element).text(result.attempts);
-        $(element).find('.weight').html('Набрано баллов: <span class="points"></span>');
+        $(element).find('.weight').html('Набрано баллов: <span class="points"></span> из <span class="max_points"></span>');
         $('.points', element).text(result.correct);
+        $('.max_points', element).text(result.weight);
 
         if (result.max_attempts && result.max_attempts <= result.attempts) {
             $('.send_button', element).html('<p><strong>Попытки исчерпаны</strong></p>')
