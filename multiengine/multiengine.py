cfc5cc36b930f4a4f560121394040382bb67089e
MasterGowen <mastergowen@gmail.com>
2015-09-30 19:59:24 +0500

    import copy

--- multiengine/multiengine.py
+++ multiengine/multiengine.py
@@ -8,9 +8,8 @@ import pytz
 import json
 import os
 from path import path
-import git
-# import shutil
 import logging
+import copy
 
 from django.template import Context, Template
 from django.utils.encoding import smart_text
