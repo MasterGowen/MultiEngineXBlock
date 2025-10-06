c2ce3dafee213020c2d7e35ced67892462eecb54
MasterGowen <5121367+MasterGowen@users.noreply.github.com>
2025-10-06 21:27:44 +0500

    Update setup.py
    upd version

--- setup.py
+++ setup.py
@@ -22,16 +22,15 @@ def package_data(pkg, roots):
 
 setup(
     name='multiengine-xblock',
-    version='0.2',
+    version='0.3',
     description='multiengine XBlock',
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Framework :: Django :: 3.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Affero General Public License v3',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
     ],
     packages=[
         'multiengine',
